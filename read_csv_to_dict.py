e7a07c8628e62ee9e7d7993783b6e2daffaad1e4
Kamal <kjdodiya@gmail.com>
2017-12-07 03:00:35 +0530

    REST api to get gene names for given species and initial letters of gene name.

---
+++ read_csv_to_dict.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+db_config = {}
+gsa_home = os.environ.get('GSA_HOME')
+if gsa_home is not None:
+	dbs_path = os.path.join(gsa_home,"settings","db_settings.csv")
+	with open(dbs_path) as dbsf:
+		dbs_reader = csv.DictReader(dbsf)
+		db_config = dbs_reader.next()
+else:
+	dbs_path = os.path.join("settings","db_settings.csv")
+	print dbs_path
+	with open(dbs_path) as dbsf:
+		dbs_reader = csv.DictReader(dbsf)
+		db_config = dbs_reader.next()
+
+#print "GSA_HOME environment variable not set. Set GSA_HOME and run appliction again"
+#print "To set GSA_HOME type export GSA_HOME=<app_home_directory>"
